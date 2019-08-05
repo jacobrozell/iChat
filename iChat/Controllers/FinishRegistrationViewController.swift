e9c3929bee20c327b4ecc578f6b9a5c7dc2a23e9
jacobrozell <jacob.rozell83@gmail.com>
2019-08-05 01:34:54 -0500

    Added login

---
+++ iChat/Controllers/FinishRegistrationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FinishRegistrationViewController.swift
+//  iChat
+//
+//  Created by Jacob Rozell on 8/5/19.
+//  Copyright © 2019 Jacob Rozell. All rights reserved.
+//
+
+import UIKit
+
+class FinishRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
